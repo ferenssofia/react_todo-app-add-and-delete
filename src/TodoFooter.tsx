f9a9b552b8bb3e9164cdd93d26bacb8412de9892
Sofia Ferens <ferenssofia3009@gmail.com>
2026-08-11 14:52:51 +0300

    Solution

---
+++ src/TodoFooter.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Status } from './types/Status';
+
+interface Props {
+  activeTodosCount: number;
+  completedTodosCount: number;
+  filter: Status;
+  setFilter: (status: Status) => void;
+  handleClearCompleted: () => void;
+}
+
+export const TodoFooter: React.FC<Props> = ({
+  activeTodosCount,
+  completedTodosCount,
+  filter,
+  setFilter,
+  handleClearCompleted,
+}) => {
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      <span className="todo-count" data-cy="TodosCounter">
+        {`${activeTodosCount} items left`}
+      </span>
+
+      <nav className="filter" data-cy="Filter">
+        <a
+          href="#/"
+          className={classNames('filter__link', {
+            selected: filter === Status.All,
+          })}
+          data-cy="FilterLinkAll"
+          onClick={() => setFilter(Status.All)}
+        >
+          All
+        </a>
+
+        <a
+          href="#/active"
+          className={classNames('filter__link', {
+            selected: filter === Status.Active,
+          })}
+          data-cy="FilterLinkActive"
+          onClick={() => setFilter(Status.Active)}
+        >
+          Active
+        </a>
+
+        <a
+          href="#/completed"
+          className={classNames('filter__link', {
+            selected: filter === Status.Completed,
+          })}
+          data-cy="FilterLinkCompleted"
+          onClick={() => setFilter(Status.Completed)}
+        >
+          Completed
+        </a>
+      </nav>
+
+      <button
+        type="button"
+        className="todoapp__clear-completed"
+        data-cy="ClearCompletedButton"
+        disabled={completedTodosCount === 0}
+        onClick={handleClearCompleted}
+      >
+        Clear completed
+      </button>
+    </footer>
+  );
+};
